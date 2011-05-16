ae5ba2d2a88d022fe0e23fa689d2a3dbce38b3c4
jweber <john@jweber.org>
2011-05-16 15:52:20 -0500

    Ignoring HTML5 and the Password element

--- jquery.hotkeys.js
+++ jquery.hotkeys.js
@@ -41,7 +41,8 @@
 		}
 		
 		var origHandler = handleObj.handler,
-			keys = handleObj.data.toLowerCase().split(" ");
+			keys = handleObj.data.toLowerCase().split(" "),
+			textAcceptingInputTypes = ["text", "password", "number", "email", "url", "range", "date", "month", "week", "time", "datetime", "datetime-local", "search", "color"];
 	
 		var sequences = [];
 		jQuery.each( keys, function()
@@ -54,7 +55,7 @@
 		handleObj.handler = function( event ) {
 			// Don't fire in text-accepting inputs that we didn't directly bind to
 			if ( this !== event.target && (/textarea|select/i.test( event.target.nodeName ) ||
-				 event.target.type === "text") ) {
+				 jQuery.inArray(event.target.type, textAcceptingInputTypes) > -1 ) ) {
 				return;
 			}
 			
